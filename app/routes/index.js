08755cc7302e571e4d21c9c0fe52c62e2c2c76bb
Claus Klingberg <cjk@pobox.com>
2014-12-16 18:17:05 +0100

    Separate index and item-index pages / templates: Create index-page with items to add to an order and the items-index to manage (add/delete) items.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    this.transitionTo('items');
+  model: function() {
+    return this.store.find('item');
+    // Later, when you need both the order and the items model:
+    // return Ember.RSVP.hash({
+    //   items: this.store.find('user'),
+    //   orders: this.store.find('tweet')
+    // });
   }
 });
